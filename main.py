7e0ee82bddc65277950282c8309c6a495470647a
LektorRichvoldsen <erria002@osloskolen.no>
2024-04-25 17:50:26 +0200

    Til 51:00

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ pygame.display.set_caption("Brawler")
 clock = pygame.time.Clock()
 FPS = 60
 
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+
 #load background image
 bg_image = pygame.image.load("assets/images/background/background.jpg").convert_alpha()
 
@@ -22,6 +26,13 @@ bg_image = pygame.image.load("assets/images/background/background.jpg").convert_
 def draw_bg():
     scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
     screen.blit(scaled_bg, (0,0))
+
+#function for drawing fighter health bar
+def draw_health_bar(health, x, y):
+    ratio = health / 100   
+    pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 404, 34))
+    pygame.draw.rect(screen, RED, (x, y, 400, 30))
+    pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
 
 
 #create two fighters
@@ -37,8 +48,12 @@ while run:
     #draw background
     draw_bg()
 
+    #show player stats
+    draw_health_bar(fighter_1.health, 20, 20)
+    draw_health_bar(fighter_2.health, 580, 20)
+
     #move fighters
-    fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT)
+    fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2)
 
 
     #draw fighters
